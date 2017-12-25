1b5cdb3a42c22115b694bb9e8167ccb27759bee0
mazemax <to.msaads@gmail.com>
2017-12-25 22:15:51 +0800

    Fixed server instance issue.

--- acoapp/networkSimulator.js
+++ acoapp/networkSimulator.js
@@ -1,21 +1,48 @@
-var arp = require('arp-a')
-    , tbl = { ipaddrs: {}, ifnames : {} }
-;
+var arp = require('arp-a'),
+    neighbours = [];
+var net = require('net');
+var address,
+    ifaces = require('os').networkInterfaces();
 
-module.exports = function networkSimulator() {
-    console.log('Network Simulator Started...');
+module.exports = function networkSimulator(server) {
+    setTimeout(function(){
+        console.log('Network Simulator Started...');
 
-    // aquire information about neighbors
-    arp.table(function(err, entry) {
-        if (!!err) return console.log('arp: ' + err.message);
-        if (!entry) return;
+        // Iterate over interfaces ...
+        for (var dev in ifaces) {
+            ifaces[dev].filter(function(details) {
+                (details.family === 'IPv4' && details.internal === false) ? address = details.address: undefined;
+            });
+        }
+        console.log("Current IP/port");
+        console.log(address);
 
-        console.log(entry);
-        tbl.ipaddrs[entry.ip] = entry.mac;
-        if (!tbl.ifnames[entry.iface]) tbl.ifnames[entry.iface] = {};
-        tbl.ifnames[entry.iface][entry.mac] = entry.ip;
-        console.log(tbl);
-    });
+        // aquire information about neighbors
+        arp.table(function(err, entry) {
+            if (!!err) return console.log('arp: ' + err.message);
+            if (!entry) return;
 
-    console.log('Network Simulator Ended...');
+            console.log(entry);
+            neighbours.push(entry.ip);
+            console.log(neighbours);
+        });
+
+        // Send data
+        // var client = new net.Socket();
+        // client.connect(8080, '127.0.0.1', function() {
+        //     console.log('Connected');
+        //     client.write('Hello, server! Love, Client.');
+        // });
+        //
+        // client.on('data', function(data) {
+        //     console.log('Received: ' + data);
+        //     client.destroy(); // kill client after server's response
+        // });
+        //
+        // client.on('close', function() {
+        //     console.log('Connection closed');
+        // });
+
+        console.log('Network Simulator Ended...');
+    }, 5000, server);
 };
